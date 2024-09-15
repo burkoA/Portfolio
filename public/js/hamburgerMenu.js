7ea545d2dcd04990aec1a6707385e5e1f90c847e
Arsen Burko <arsenburko67@gmail.com>
2024-09-15 10:51:52 +0200

    Fix Hamburger Menu & Fix Responsive Screen

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -1,13 +1,39 @@
-document.getElementById('menu-button').addEventListener('click', function() {
-    document.getElementById('mobile-menu').classList.remove('translate-x-full');
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenuButton = document.getElementById('close-menu');
+const body = document.body;
+
+menuButton.addEventListener('click', function() {
+    mobileMenu.classList.remove('hidden');
+    mobileMenu.classList.add('block');
+    
+    body.classList.add('no-scroll');
+    
+    menuButton.children[0].classList.toggle('rotate-45');
+    menuButton.children[1].classList.toggle('opacity-0');
+    menuButton.children[2].classList.toggle('-rotate-45');
 });
 
-document.getElementById('close-menu').addEventListener('click', function() {
-    document.getElementById('mobile-menu').classList.add('translate-x-full');
+closeMenuButton.addEventListener('click', function() {
+    mobileMenu.classList.remove('block');
+    mobileMenu.classList.add('hidden');
+    
+    body.classList.remove('no-scroll');
+    
+    menuButton.children[0].classList.remove('rotate-45');
+    menuButton.children[1].classList.remove('opacity-0');
+    menuButton.children[2].classList.remove('-rotate-45');
 });
 
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', function() {
-        document.getElementById('mobile-menu').classList.add('translate-x-full');
+        mobileMenu.classList.remove('block');
+        mobileMenu.classList.add('hidden');
+        
+        body.classList.remove('no-scroll');
+        
+        menuButton.children[0].classList.remove('rotate-45');
+        menuButton.children[1].classList.remove('opacity-0');
+        menuButton.children[2].classList.remove('-rotate-45');
     });
 });
